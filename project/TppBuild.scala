182abdb196c41f107960228be2352688d0ba8d4d
takashabe <takashabe.public@gmail.com>
2012-10-21 20:12:04 +0900

    pluginにspecs2を追加した。

--- project/TppBuild.scala
+++ project/TppBuild.scala
@@ -10,7 +10,18 @@ object TppBuild extends Build {
       name := "tpp",
       organization := "com.takashabe.tpp",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.9.2"
+      scalaVersion := "2.9.2",
+      resolvers ++= myResolvers,
+      libraryDependencies ++= myLibraryDependecies
     )
   )
+
+  val myLibraryDependecies = Seq (
+    "org.specs2" %% "specs2" % "1.12.2" % "test"
+  )
+
+  val myResolvers = Seq (
+    "release" at "http://oss.sonatype.org/content/repositories/release"
+  )
+
 }
